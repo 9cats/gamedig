414906aceb36ae02563d42eb52aadb4af8174aec
9cats <123337671@qq.com>
2023-03-14 18:10:14 +0800

    init commit

---
+++ pages/api/index.ts
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import Gamedig from "gamedig";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const { queryOptions }: { queryOptions: Gamedig.QueryOptions } = req.body;
+
+  try {
+    const data = await Gamedig.query(queryOptions);
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json(e);
+  }
+}
