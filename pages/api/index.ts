358c34123b5de9f66b4d6e14d049866569ac6379
9cats <123337671@qq.com>
2023-03-14 18:17:53 +0800

    add console.log

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -8,10 +8,13 @@ export default async function handler(
 ) {
   const { queryOptions }: { queryOptions: Gamedig.QueryOptions } = req.body;
 
+  console.log(queryOptions);
   try {
     const data = await Gamedig.query(queryOptions);
+    console.log(data);
     res.status(200).json(data);
   } catch (e) {
+    console.log(e);
     res.status(500).json(e);
   }
 }
