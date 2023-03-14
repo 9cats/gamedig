9789daf37986394bb25c8843afbf28f37b422cd9
9cats <123337671@qq.com>
2023-03-14 19:32:43 +0800

    update

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -6,15 +6,11 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  const { queryOptions }: { queryOptions: Gamedig.QueryOptions } = req.body;
-
-  console.log(queryOptions);
+  const queryOptions : Gamedig.QueryOptions  = req.body;
   try {
     const data = await Gamedig.query(queryOptions);
-    console.log(data);
     res.status(200).json(data);
   } catch (e) {
-    console.log(e);
     res.status(500).json(e);
   }
 }
